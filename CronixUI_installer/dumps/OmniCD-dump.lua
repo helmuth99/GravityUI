a9d9155e42d63942f219d4dcd47af1d8da07f015
Helmuth99 <helmuth.petelin@hotmail.de>
2024-08-26 15:45:00 +0200

    fix: :bug: fixed a bug that  OmniCd didnt have a profile
    fixed a bug that  OmniCd didnt have a profile

--- CronixUI_installer/dumps/OmniCD-dump.lua
+++ CronixUI_installer/dumps/OmniCD-dump.lua
@@ -1,294 +1,216 @@
 Addon, private = ...
 
 local CronixUI = {
-    ["profileKeys"] = {
-        ["Crónix - Mal'Ganis"] = "Default",
-        ["Evonix - Blackhand"] = "Default",
-        ["Venøm - Mal'Ganis"] = "Default",
-        ["Cronixqq - Mal'Ganis"] = "Default",
-        ["Crônix - Blackhand"] = "Default",
-        ["Cnxwarrior - Blackhand"] = "Default",
-        ["Gravîty - Blackhand"] = "Default",
-        ["Cronîx - Blackhand"] = "croniXUI",
-        ["Cronixe - Blackhand"] = "Default",
-        ["Cronolie - Blackhand"] = "Default",
-        ["Cnxmonk - Blackhand"] = "Default",
-        ["Crônîx - Blackhand"] = "Default",
-        ["Cronìx - Blackhand"] = "croniXUI",
-    },
-    ["namespaces"] = {
-        ["LibDualSpec-1.0"] = {
-            ["char"] = {
-                ["Atrocitys - Illidan"] = {
-                    "AtrocityUI",
-                    "AtrocityUI",
-                    "AtrocityUI Healer",
-                    ["enabled"] = true,
-                },
-                ["Atrocitydr - Illidan"] = {
-                    "AtrocityUI",
-                    "AtrocityUI",
-                    "AtrocityUI",
-                    "AtrocityUI Healer",
-                    ["enabled"] = true,
-                },
-                ["Atrocityp - Illidan"] = {
-                    "AtrocityUI",
-                    "AtrocityUI",
-                    "AtrocityUI",
-                    ["enabled"] = true,
-                },
-                ["Atrocityjr - Illidan"] = {
-                    "AtrocityUI Healer",
-                    "AtrocityUI",
-                    "AtrocityUI",
-                    ["enabled"] = true,
-                },
-                ["Atrocity - Illidan"] = {
-                    ["enabled"] = false,
-                },
-                ["Atrocitykfk - Illidan"] = {
-                    ["enabled"] = false,
-                },
-                ["Cronîx - Blackhand"] = {
-                    ["enabled"] = false,
-                },
+    ["General"] = {
+        ["textures"] = {
+            ["statusBar"] = {
+                ["BG"] = "Cronix",
+                ["bar"] = "Cronix",
             },
         },
-        ["BattleRes"] = {
-            ["profiles"] = {
-                ["AtrocityUI"] = {
+        ["fonts"] = {
+            ["statusBar"] = {
+                ["flag"] = "OUTLINE",
+                ["font"] = "Cronix",
+                ["size"] = 18,
+            },
+            ["icon"] = {
+                ["font"] = "Cronix",
+                ["ofsX"] = 1,
+                ["size"] = 9,
+            },
+            ["anchor"] = {
+                ["flag"] = "OUTLINE",
+                ["font"] = "Cronix",
+            },
+        },
+    },
+    ["Party"] = {
+        ["visibility"] = {
+            ["size"] = 40,
+            ["arena"] = false,
+        },
+        ["party"] = {
+            ["extraBars"] = {
+                ["raidBar2"] = {
+                    ["enabled"] = true,
                     ["manualPos"] = {
-                        ["OmniCDBrez"] = {
-                            ["y"] = 407.1999431769073,
-                            ["x"] = 365.600238625193,
+                        ["raidBar2"] = {
+                            ["y"] = 313.3668581552047,
+                            ["x"] = 299.9668698297683,
                         },
                     },
+                    ["locked"] = true,
+                    ["layout"] = "horizontal",
+                    ["columns"] = 5,
+                    ["scale"] = 0.9199999999999999,
                 },
-            },
-        },
-    },
-    ["cooldowns"] = {
-    },
-    ["version"] = 4,
-    ["global"] = {
-        ["disableElvMsg"] = true,
-        ["oodChk"] = 221021,
-        ["oodVer"] = 9272723,
-    },
-    ["profiles"] = {
-        ["Cronîx - Blackhand"] = {
-        },
-        ["croniXUI"] = {
-            ["Party"] = {
-                ["party"] = {
-                    ["extraBars"] = {
+                ["raidBar1"] = {
+                    ["showRaidTargetMark"] = true,
+                    ["growUpward"] = true,
+                    ["locked"] = true,
+                    ["manualPos"] = {
                         ["raidBar1"] = {
-                            ["manualPos"] = {
-                                ["raidBar1"] = {
-                                    ["y"] = 406.6999307115839,
-                                    ["x"] = 299.4340695156279,
-                                },
-                            },
-                            ["locked"] = true,
+                            ["y"] = 417.3666669752856,
+                            ["x"] = 299.9674374567439,
                         },
                     },
-                    ["general"] = {
-                        ["showPlayer"] = true,
-                    },
-                    ["spells"] = {
-                        ["363916"] = false,
-                        ["216331"] = false,
-                        ["118038"] = false,
-                        ["198589"] = false,
-                        ["1022"] = false,
-                        ["8122"] = false,
-                        ["235219"] = false,
-                        ["5246"] = false,
-                        ["221562"] = true,
-                        ["115203"] = false,
-                        ["209258"] = false,
-                        ["31224"] = false,
-                        ["404381"] = false,
-                        ["122783"] = false,
-                        ["231895"] = false,
-                        ["48707"] = false,
-                        ["363534"] = false,
-                        ["61336"] = false,
-                        ["265202"] = false,
-                        ["45438"] = false,
-                        ["31230"] = false,
-                        ["328530"] = true,
-                        ["31935"] = false,
-                        ["186265"] = false,
-                        ["48792"] = false,
-                        ["203720"] = true,
-                        ["122470"] = false,
-                        ["23920"] = false,
-                        ["19574"] = true,
-                        ["108968"] = false,
-                        ["200806"] = true,
-                        ["108280"] = false,
-                        ["360194"] = false,
-                        ["342245"] = false,
-                        ["374251"] = true,
-                        ["42650"] = true,
-                        ["193530"] = true,
-                        ["871"] = false,
-                        ["47536"] = false,
-                        ["64843"] = false,
-                        ["86949"] = false,
-                        ["114556"] = false,
-                        ["5277"] = false,
-                        ["740"] = false,
-                        ["196555"] = false,
-                        ["322118"] = true,
-                        ["47585"] = false,
-                        ["642"] = false,
-                        ["122278"] = false,
-                    },
-                    ["icons"] = {
-                        ["scale"] = 0.98,
-                        ["desaturateActive"] = true,
-                        ["markEnhanced"] = false,
-                        ["showTooltip"] = true,
-                    },
-                    ["position"] = {
-                        ["offsetX"] = 1,
-                        ["anchor"] = "TOPRIGHT",
-                        ["paddingY"] = 1,
-                        ["columns"] = 4,
-                        ["paddingX"] = 1,
-                        ["attach"] = "TOPLEFT",
-                        ["maxNumIcons"] = 4,
-                        ["preset"] = "TOPLEFT",
-                    },
-                    ["manualPos"] = {
-                        {
-                            ["y"] = 384.3550042531351,
-                            ["x"] = 682.3116694426026,
-                        },
-                        [5] = {
-                            ["y"] = 384.3550042531351,
-                            ["x"] = 682.3116694426026,
-                        },
-                    },
-                    ["highlight"] = {
-                        ["glow"] = false,
-                        ["glowBuffTypes"] = {
-                            ["offensive"] = true,
-                        },
-                    },
-                },
-                ["noneZoneSetting"] = "party",
-                ["raid"] = {
-                    ["general"] = {
-                        ["zoneSelected"] = "party",
-                        ["showPlayer"] = true,
-                        ["showPlayerEx"] = false,
-                        ["showRange"] = true,
-                    },
-                    ["spells"] = {
-                        ["374227"] = false,
-                        ["6940"] = true,
-                        ["740"] = false,
-                        ["204018"] = true,
-                        ["115310"] = false,
-                        ["196718"] = false,
-                        ["62618"] = false,
-                        ["108280"] = false,
-                        ["363534"] = false,
-                        ["196555"] = false,
-                        ["51052"] = false,
-                        ["64843"] = false,
-                        ["10060"] = true,
-                        ["45438"] = false,
-                        ["642"] = false,
-                        ["98008"] = false,
-                        ["265202"] = false,
-                        ["388007"] = true,
-                        ["97462"] = false,
-                        ["31821"] = false,
-                        ["186265"] = false,
-                    },
-                    ["icons"] = {
-                        ["counterScale"] = 0.7000000000000001,
-                        ["chargeScale"] = 0.9,
-                        ["scale"] = 0.96,
-                        ["desaturateActive"] = true,
-                        ["markEnhanced"] = false,
-                        ["showForbearanceCounter"] = false,
-                    },
-                    ["raidCDS"] = {
-                        ["199452"] = true,
-                        ["47788"] = true,
-                        ["357170"] = true,
-                        ["10060"] = true,
-                        ["6940"] = true,
-                        ["33206"] = true,
-                        ["102342"] = true,
-                        ["388007"] = true,
-                        ["1022"] = true,
-                        ["204018"] = true,
-                        ["116849"] = true,
-                    },
-                    ["position"] = {
-                        ["offsetX"] = 2,
-                    },
-                    ["priority"] = {
-                        ["offensive"] = 9,
-                        ["immunity"] = 12,
-                    },
-                    ["highlight"] = {
-                        ["glowBuffs"] = false,
-                    },
-                    ["manualPos"] = {
-                        ["raidCDBar2"] = {
-                            ["y"] = 275.5551344944179,
-                            ["x"] = 45.86719465872011,
-                        },
-                    },
-                },
-                ["visibility"] = {
-                    ["arena"] = false,
-                    ["size"] = 40,
+                    ["statusBarWidth"] = 231,
                 },
             },
-            ["General"] = {
-                ["textures"] = {
-                    ["statusBar"] = {
-                        ["BG"] = "Cronix",
-                        ["bar"] = "Cronix",
-                    },
+            ["highlight"] = {
+                ["glow"] = false,
+                ["glowBuffTypes"] = {
+                    ["offensive"] = true,
                 },
-                ["fonts"] = {
-                    ["statusBar"] = {
-                        ["font"] = "Cronix",
-                        ["flag"] = "OUTLINE",
-                        ["size"] = 18,
-                    },
-                    ["icon"] = {
-                        ["font"] = "Cronix",
-                        ["ofsX"] = 1,
-                        ["size"] = 9,
-                    },
-                    ["anchor"] = {
-                        ["font"] = "Cronix",
-                        ["flag"] = "OUTLINE",
-                    },
+            },
+            ["position"] = {
+                ["attach"] = "TOPLEFT",
+                ["maxNumIcons"] = 4,
+                ["offsetX"] = 1,
+                ["paddingX"] = 1,
+                ["anchor"] = "TOPRIGHT",
+                ["preset"] = "TOPLEFT",
+                ["columns"] = 4,
+                ["paddingY"] = 1,
+            },
+            ["general"] = {
+                ["showPlayer"] = true,
+            },
+            ["spells"] = {
+                ["328530"] = true,
+                ["363916"] = false,
+                ["216331"] = false,
+                ["209258"] = false,
+                ["118038"] = false,
+                ["198589"] = false,
+                ["1022"] = false,
+                ["193530"] = true,
+                ["404381"] = false,
+                ["48792"] = false,
+                ["322118"] = true,
+                ["122783"] = false,
+                ["86949"] = false,
+                ["203720"] = true,
+                ["122470"] = false,
+                ["48707"] = false,
+                ["23920"] = false,
+                ["31224"] = false,
+                ["231895"] = false,
+                ["196555"] = false,
+                ["360194"] = false,
+                ["8122"] = false,
+                ["235219"] = false,
+                ["47536"] = false,
+                ["363534"] = false,
+                ["19574"] = true,
+                ["64843"] = false,
+                ["5277"] = false,
+                ["61336"] = false,
+                ["200806"] = true,
+                ["871"] = false,
+                ["122278"] = false,
+                ["740"] = false,
+                ["5246"] = false,
+                ["342245"] = false,
+                ["265202"] = false,
+                ["108280"] = false,
+                ["47585"] = false,
+                ["114556"] = false,
+                ["221562"] = true,
+                ["45438"] = false,
+                ["642"] = false,
+                ["186265"] = false,
+                ["115203"] = false,
+                ["31935"] = false,
+                ["31230"] = false,
+                ["374251"] = true,
+                ["108968"] = false,
+            },
+            ["icons"] = {
+                ["scale"] = 0.98,
+                ["desaturateActive"] = true,
+                ["markEnhanced"] = false,
+                ["showTooltip"] = true,
+            },
+            ["manualPos"] = {
+                {
+                    ["y"] = 384.3550042531351,
+                    ["x"] = 682.3116694426026,
+                },
+                [5] = {
+                    ["y"] = 384.3550042531351,
+                    ["x"] = 682.3116694426026,
                 },
             },
         },
-        ["Default"] = {
-            ["Party"] = {
-                ["party"] = {
-                    ["manualPos"] = {
-                        ["interruptBar"] = {
-                            ["y"] = 384.2666763146699,
-                            ["x"] = 682.4000244299532,
-                        },
-                    },
+        ["noneZoneSetting"] = "party",
+        ["raid"] = {
+            ["raidCDS"] = {
+                ["199452"] = true,
+                ["47788"] = true,
+                ["357170"] = true,
+                ["10060"] = true,
+                ["6940"] = true,
+                ["33206"] = true,
+                ["102342"] = true,
+                ["388007"] = true,
+                ["1022"] = true,
+                ["204018"] = true,
+                ["116849"] = true,
+            },
+            ["highlight"] = {
+                ["glowBuffs"] = false,
+            },
+            ["manualPos"] = {
+                ["raidCDBar2"] = {
+                    ["y"] = 275.5551344944179,
+                    ["x"] = 45.86719465872011,
                 },
+            },
+            ["position"] = {
+                ["offsetX"] = 2,
+            },
+            ["general"] = {
+                ["zoneSelected"] = "party",
+                ["showPlayer"] = true,
+                ["showPlayerEx"] = false,
+                ["showRange"] = true,
+            },
+            ["spells"] = {
+                ["374227"] = false,
+                ["6940"] = true,
+                ["740"] = false,
+                ["204018"] = true,
+                ["196718"] = false,
+                ["62618"] = false,
+                ["186265"] = false,
+                ["363534"] = false,
+                ["196555"] = false,
+                ["51052"] = false,
+                ["64843"] = false,
+                ["10060"] = true,
+                ["115310"] = false,
+                ["45438"] = false,
+                ["642"] = false,
+                ["98008"] = false,
+                ["265202"] = false,
+                ["388007"] = true,
+                ["97462"] = false,
+                ["108280"] = false,
+                ["31821"] = false,
+            },
+            ["icons"] = {
+                ["counterScale"] = 0.7000000000000001,
+                ["chargeScale"] = 0.9,
+                ["scale"] = 0.96,
+                ["showForbearanceCounter"] = false,
+                ["markEnhanced"] = false,
+                ["desaturateActive"] = true,
+            },
+            ["priority"] = {
+                ["offensive"] = 9,
+                ["immunity"] = 12,
             },
         },
     },
