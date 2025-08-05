e4365a4dc5af59ba0fda14c465ccbc14c57c782f
Helmuth99 <helmuth.petelin@hotmail.de>
2025-08-05 21:40:59 +0200

    fixed an error with hidingbar

--- GravityUI/Register/HidingBar.lua
+++ GravityUI/Register/HidingBar.lua
@@ -6,13 +6,13 @@ local addon, private = ...
 name = addonName
 import = importLink this needs to be a function
 importText = importText Text that will be on the button
-importTwink = Twinkinstallation Process 
+importTwink = Twinkinstallation Process
 }
 ]]
 local addonName = "HidingBar"
 local importText = "Import \n!!GLOBAL OVERWRITE!!"
 local data = {
-    ["tstmp"] = 1754398718,
+    ["tstmp"] = 1754420999,
     ["profiles"] = {
         {
             ["isDefault"] = true,
@@ -24,35 +24,35 @@ local data = {
                     },
                     ["BtWQuestsMinimapButton"] = {
                         true,
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
                     ["LibDBIcon10_KeystoneGroupList"] = {
                         true,
-                        ["tstmp"] = 1754397053,
+                        ["tstmp"] = 1754409943,
                     },
                     ["LibDBIcon10_VocalRaidAssistant"] = {
                         true,
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
                     ["LibDBIcon10_MythicDungeonTools"] = {
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
                     ["LibDBIcon10_BugSack"] = {
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
                     ["LibDBIcon10_Socialite"] = {
                         ["tstmp"] = 1748255535,
                     },
                     ["LibDBIcon10_Details"] = {
                         true,
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
                     ["LibDBIcon10_Latency"] = {
                         ["tstmp"] = 1748479760,
                     },
                     ["LibDBIcon10_MethodRaidTools"] = {
                         true,
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
                     ["LibDBIcon10_Guild"] = {
                         ["tstmp"] = 1748258624,
@@ -72,83 +72,86 @@ local data = {
                     },
                     ["LibDBIcon10_RaiderIO"] = {
                         true,
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
                     ["AddonCompartmentFrame"] = {
                         true,
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
-                    ["LibDBIcon10_ClassSpecs"] = {
-                        ["tstmp"] = 1748480849,
+                    ["_G.ExpansionLandingPageMinimapButton"] = {
+                        ["tstmp"] = 1754420999,
                     },
-                    ["LibDBIcon10_Durability"] = {
-                        ["tstmp"] = 1748479772,
+                    ["LibDBIcon10_FPS"] = {
+                        ["tstmp"] = 1748479749,
                     },
                     ["MinimapButton_D4Lib_LibDBIcon_MoveAny"] = {
                         ["tstmp"] = 1748940699,
                     },
+                    ["LibDBIcon10_Plater"] = {
+                        true,
+                        ["tstmp"] = 1754420999,
+                    },
                     ["LibDBIcon10_Leatrix_Plus"] = {
                         false,
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
                     ["LibDBIcon10_Masque"] = {
                         true,
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
-                    ["LibDBIcon10_GPS"] = {
-                        ["tstmp"] = 1748258624,
+                    ["LibDBIcon10_ClassSpecs"] = {
+                        ["tstmp"] = 1748480849,
                     },
                     ["LibDBIcon10_System"] = {
                         ["tstmp"] = 1748258624,
                     },
-                    ["LibDBIcon10_Gold"] = {
-                        ["tstmp"] = 1748258624,
+                    ["LibDBIcon10_SylingTracker"] = {
+                        ["tstmp"] = 1747421324,
                     },
                     ["LibDBIcon10_AutoQueueWA"] = {
                         true,
                         ["tstmp"] = 1752443888,
                     },
-                    ["LibDBIcon10_Bartender4"] = {
-                        true,
-                        ["tstmp"] = 1750200829,
-                    },
                     ["LibDBIcon10_BigWigs"] = {
                         true,
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
+                    },
+                    ["LibDBIcon10_NSRT"] = {
+                        true,
+                        ["tstmp"] = 1754420999,
                     },
                     ["LibDBIcon10_Volume"] = {
                         ["tstmp"] = 1748258624,
                     },
                     ["LibDBIcon10_Dominos"] = {
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
                     ["LibDBIcon10_Equipment"] = {
                         ["tstmp"] = 1748258624,
                     },
-                    ["LibDBIcon10_NSRT"] = {
+                    ["LibDBIcon10_Bartender4"] = {
                         true,
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1750200829,
                     },
                     ["LibDBIcon10_Bags"] = {
                         ["tstmp"] = 1748258624,
                     },
                     ["LibDBIcon10_SimulationCraft"] = {
                         true,
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
-                    ["LibDBIcon10_SylingTracker"] = {
-                        ["tstmp"] = 1747421324,
+                    ["LibDBIcon10_Gold"] = {
+                        ["tstmp"] = 1748258624,
                     },
                     ["LibDBIcon10_WeakAuras"] = {
                         true,
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
-                    ["LibDBIcon10_Plater"] = {
-                        true,
-                        ["tstmp"] = 1754398718,
+                    ["LibDBIcon10_GPS"] = {
+                        ["tstmp"] = 1748258624,
                     },
-                    ["LibDBIcon10_FPS"] = {
-                        ["tstmp"] = 1748479749,
+                    ["LibDBIcon10_Durability"] = {
+                        ["tstmp"] = 1748479772,
                     },
                     ["LibDBIcon10_TipTac"] = {
                         true,
@@ -164,17 +167,17 @@ local data = {
                     },
                     ["HidingBar"] = {
                         true,
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
                     ["BigWigs"] = {
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
-                    ["MRT"] = {
-                        ["tstmp"] = 1754398718,
+                    ["RaiderIO"] = {
+                        ["tstmp"] = 1754420999,
                     },
                     ["BtWQuests"] = {
                         true,
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
                     ["Bartender4"] = {
                         ["tstmp"] = 1750200829,
@@ -183,36 +186,37 @@ local data = {
                         ["tstmp"] = 1747930734,
                     },
                     ["Dominos"] = {
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
                     ["NSRT"] = {
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
                     ["Masque"] = {
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
                     ["Prat"] = {
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
                     ["VocalRaidAssistant"] = {
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
                     ["TipTac"] = {
                         ["tstmp"] = 1747441096,
                     },
                     ["WeakAuras"] = {
-                        ["tstmp"] = 1754398718,
+                        ["tstmp"] = 1754420999,
                     },
-                    ["RaiderIO"] = {
-                        ["tstmp"] = 1754398718,
+                    ["MRT"] = {
+                        ["tstmp"] = 1754420999,
                     },
                 },
                 ["grabMinimap"] = true,
                 ["grabMinimapAfterN"] = 1,
-                ["ombGrabQueue"] = {
-                },
                 ["customGrabList"] = {
                     "AddonCompartmentFrame",
+                    "_G.ExpansionLandingPageMinimapButton",
+                },
+                ["ombGrabQueue"] = {
                 },
                 ["ignoreMBtn"] = {
                     "GatherMatePin",
@@ -235,13 +239,13 @@ local data = {
                         ["showDelay"] = 0,
                         ["bgTexture"] = "Solid",
                         ["borderColor"] = {
-                            1,
-                            1,
-                            1,
+                            0,
+                            0,
+                            0,
                             1,
                         },
                         ["anchor"] = "right",
-                        ["lineTexture"] = "Solid",
+                        ["lineTexture"] = "Cronix",
                         ["barTypePosition"] = 0,
                         ["size"] = 15,
                         ["interceptTooltipPosition"] = 0,
@@ -251,44 +255,44 @@ local data = {
                             0.7333333333333333,
                             1,
                         },
-                        ["position"] = 617.7780395790468,
+                        ["position"] = 606.8668087826882,
                         ["mbtnPosition"] = 2,
                         ["lineBorderEdge"] = false,
                         ["lineBorderOffset"] = 1,
                         ["showHandler"] = 2,
                         ["expand"] = 2,
-                        ["borderEdge"] = false,
+                        ["borderEdge"] = "1 Pixel",
                         ["bgColor"] = {
                             0.1,
                             0.1,
                             0.1,
                             0.7,
                         },
-                        ["borderSize"] = 16,
-                        ["gapSize"] = 0,
+                        ["borderSize"] = 12,
+                        ["gapSize"] = 1,
                         ["interceptTooltip"] = true,
                         ["buttonDirection"] = {
-                            ["H"] = 0,
                             ["V"] = 0,
+                            ["H"] = 0,
                         },
                         ["borderOffset"] = 4,
                         ["omb"] = {
-                            ["size"] = 31,
+                            ["distanceToBar"] = 0,
                             ["hide"] = true,
                             ["fadeOpacity"] = 1,
                             ["anchor"] = "right",
                             ["barDisplacement"] = 0,
                             ["canGrabbed"] = false,
-                            ["distanceToBar"] = 0,
+                            ["size"] = 31,
                         },
-                        ["buttonSize"] = 31,
+                        ["buttonSize"] = 34,
                         ["frameStrata"] = 2,
                         ["lineBorderSize"] = 2,
                         ["fadeOpacity"] = 0.2,
                         ["orientation"] = 0,
-                        ["rangeBetweenBtns"] = 0,
-                        ["hideDelay"] = 0.75,
                         ["barOffset"] = 2,
+                        ["hideDelay"] = 0.75,
+                        ["rangeBetweenBtns"] = 0,
                     },
                     ["name"] = "Bar 1",
                 },
