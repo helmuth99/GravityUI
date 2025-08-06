d87f9e32c3ec5b005027431069c0efd52cc35480
Helmuth99 <helmuth.petelin@hotmail.de>
2025-08-07 01:23:19 +0200

    Updated Skinner

--- GravityUI/Register/Skinner.lua
+++ GravityUI/Register/Skinner.lua
@@ -5,63 +5,33 @@ local addon, private = ...
 name = addonName
 import = importLink this needs to be a function
 importText = importText Text that will be on the button
-importTwink = Twinkinstallation Process 
+importTwink = Twinkinstallation Process
 }
 ]]
 local addonName = "Skinner"
 local importText = "Import"
 local data = {
-    ["QuestInfo"] = false,
     ["MirrorTimers"] = {
         ["glaze"] = false,
     },
     ["PVPMatch"] = false,
-    ["QuestFrame"] = false,
     ["CastingBar"] = {
-        ["skin"] = false,
         ["glaze"] = false,
+        ["skin"] = false,
     },
     ["Backdrop"] = {
-        ["a"] = 0.7551859021186829,
+        ["a"] = 0.708888590335846,
     },
-    ["DisabledSkins"] = {
-        ["Simulationcraft"] = true,
-        ["Prat-3.0"] = true,
-        ["BugSack"] = true,
-        ["Leatrix_Plus"] = true,
-        ["LibDBIcon-1.0 (Lib)"] = true,
-        ["LibDropDown (Lib)"] = true,
-        ["WeakAurasOptions (LoD)"] = true,
-        ["AceGUI-3.0 (Lib)"] = true,
-        ["Bartender4"] = true,
-        ["MSA-DropDownMenu-1.0 (Lib)"] = true,
-        ["AdvancedInterfaceOptions"] = true,
-        ["Baganator"] = true,
-        ["ColorPickerPlus"] = true,
-        ["WeakAuras"] = true,
-        ["Dominos_Config (LoD)"] = true,
-        ["RCLootCouncil"] = true,
-        ["BigWigs"] = true,
-        ["WorldQuestTracker"] = true,
-        ["LibKeyBound-1.0 (Lib)"] = true,
-        ["Details"] = true,
-        ["BtWQuests"] = true,
-        ["DetailsFramework-1.0 (Lib)"] = true,
-        ["Syndicator"] = true,
-        ["TomTom"] = true,
-        ["Postal"] = true,
-        ["Auctionator"] = true,
-        ["RaiderIO"] = true,
-        ["NickTag-1.0 (Lib)"] = true,
-        ["LibDialog-1.1 (Lib)"] = true,
-        ["ScrollingTable (Lib)"] = true,
-        ["BetterWardrobe"] = true,
+    ["GradientMin"] = {
+        ["b"] = 0.1019607931375504,
+        ["g"] = 0.1019607931375504,
+        ["r"] = 0.1019607931375504,
     },
     ["Gradient"] = {
-        ["enable"] = false,
+        ["addon"] = false,
         ["npc"] = false,
         ["char"] = false,
-        ["addon"] = false,
+        ["enable"] = false,
         ["ui"] = false,
         ["skinner"] = false,
     },
@@ -74,19 +44,15 @@ local data = {
         ["skin"] = true,
     },
     ["ReadyCheck"] = false,
+    ["ExpansionLandingPage"] = false,
     ["CharacterFrames"] = false,
     ["ChatBubbles"] = {
         ["skin"] = false,
     },
     ["TexturedDD"] = false,
     ["TexturedTab"] = false,
-    ["GradientMin"] = {
-        ["b"] = 0.1019607931375504,
-        ["g"] = 0.1019607931375504,
-        ["r"] = 0.1019607931375504,
-    },
     ["StatusBar"] = {
-        ["a"] = 0.3059892356395721,
+        ["a"] = 0.7018523812294006,
         ["b"] = 0.2156862914562225,
         ["r"] = 0.1960784494876862,
         ["g"] = 0.2039215862751007,
@@ -101,33 +67,62 @@ local data = {
         ["g"] = 1,
         ["r"] = 1,
     },
+    ["WeeklyRewards"] = false,
     ["BackdropBorder"] = {
         ["a"] = 0.8055558800697327,
         ["b"] = 0,
         ["g"] = 0,
         ["r"] = 0,
     },
-    ["BdDefault"] = false,
+    ["ZoneAbility"] = false,
     ["DisableAllAS"] = true,
-    ["Warnings"] = false,
-    ["TabDDTextures"] = {
-        ["tabddtex"] = "Details Ground",
+    ["DisabledSkins"] = {
+        ["Simulationcraft"] = true,
+        ["LibDialog-1.1 (Lib)"] = true,
+        ["BugSack"] = true,
+        ["Prat-3.0"] = true,
+        ["BetterWardrobe"] = true,
+        ["Leatrix_Plus"] = true,
+        ["WeakAurasOptions (LoD)"] = true,
+        ["AceGUI-3.0 (Lib)"] = true,
+        ["Dominos_Config (LoD)"] = true,
+        ["Auctionator"] = true,
+        ["AdvancedInterfaceOptions"] = true,
+        ["Baganator"] = true,
+        ["DetailsFramework-1.0 (Lib)"] = true,
+        ["WeakAuras"] = true,
+        ["RaiderIO"] = true,
+        ["RCLootCouncil"] = true,
+        ["BigWigs"] = true,
+        ["Bartender4"] = true,
+        ["LibKeyBound-1.0 (Lib)"] = true,
+        ["Details"] = true,
+        ["BtWQuests"] = true,
+        ["ColorPickerPlus"] = true,
+        ["WorldQuestTracker"] = true,
+        ["TomTom"] = true,
+        ["Postal"] = true,
+        ["MSA-DropDownMenu-1.0 (Lib)"] = true,
+        ["Syndicator"] = true,
+        ["LibDropDown (Lib)"] = true,
+        ["LibDBIcon-1.0 (Lib)"] = true,
+        ["NickTag-1.0 (Lib)"] = true,
+        ["ScrollingTable (Lib)"] = true,
     },
-    ["QuestMap"] = false,
+    ["Warnings"] = false,
+    ["Nameplates"] = false,
     ["Collections"] = false,
-    ["GossipFrame"] = false,
     ["Errors"] = false,
+    ["MajorFactions"] = false,
     ["HeadText"] = {
-        ["b"] = 1,
-        ["g"] = 1,
-        ["r"] = 1,
+        ["g"] = 0.6117647290229797,
+        ["r"] = 0.7490196228027344,
     },
     ["IgnoredText"] = {
         ["b"] = 1,
         ["g"] = 1,
         ["r"] = 1,
     },
-    ["Nameplates"] = false,
     ["GradientMax"] = {
         ["a"] = 0,
         ["b"] = 0.250980406999588,
@@ -136,46 +131,53 @@ local data = {
     },
     ["PVEFrame"] = false,
     ["PlayerSpells"] = false,
-    ["DelvesUI"] = false,
+    ["Settings"] = false,
+    ["MainMenuBar"] = {
+        ["glazesb"] = false,
+    },
+    ["BdInset"] = 0,
+    ["BdDefault"] = false,
+    ["BdBorderTexture"] = "1 Pixel",
+    ["BdTexture"] = "Solid",
+    ["CovenantRenown"] = false,
     ["SliderBorder"] = {
         ["a"] = 0.8055558800697327,
         ["b"] = 0,
         ["g"] = 0,
         ["r"] = 0,
     },
-    ["MainMenuBar"] = {
-        ["glazesb"] = false,
+    ["LFGTexture"] = true,
+    ["TabDDTextures"] = {
+        ["tabddtex"] = "Details Ground",
     },
-    ["BdInset"] = 0,
+    ["Tooltips"] = {
+        ["style"] = 3,
+        ["glazesb"] = false,
+        ["border"] = 2,
+    },
+    ["CovenantToasts"] = false,
+    ["BdEdgeSize"] = 1,
+    ["DelvesUI"] = false,
     ["TooltipBorder"] = {
         ["a"] = 0.8025929927825928,
         ["b"] = 0,
         ["g"] = 0,
         ["r"] = 0,
     },
-    ["BdBorderTexture"] = "1 Pixel",
-    ["BdTexture"] = "Solid",
-    ["LFGTexture"] = true,
-    ["Tooltips"] = {
-        ["style"] = 3,
-        ["glazesb"] = false,
-        ["border"] = 2,
-    },
-    ["BdEdgeSize"] = 1,
-    ["Settings"] = false,
+    ["EncounterJournal"] = false,
     ["HelpFrame"] = false,
 }
 
 local function install()
     if SkinnerDB and private.g.cName and private.g.cRealm then
         SkinnerDB["profiles"][private.g.name] = data
-        SkinnerDB["profileKeys"][private.g.cName.. " - "..private.g.cRealm] = private.g.name
+        SkinnerDB["profileKeys"][private.g.cName .. " - " .. private.g.cRealm] = private.g.name
     end
 end
 
 local function installTwink()
     if SkinnerDB and private.g.cName and private.g.cRealm then
-        SkinnerDB["profileKeys"][private.g.cName.. " - "..private.g.cRealm] = private.g.name
+        SkinnerDB["profileKeys"][private.g.cName .. " - " .. private.g.cRealm] = private.g.name
     end
 end
 
